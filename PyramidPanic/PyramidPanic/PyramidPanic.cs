5a7a4a1908bab31ebc3c895b44e38a6c22e7eed8
Jason Binneveld <j.binneveld@live.nl>
2013-11-28 11:41:34 +0100

    MouseVisible en escape toegevoegd

--- PyramidPanic/PyramidPanic/PyramidPanic.cs
+++ PyramidPanic/PyramidPanic/PyramidPanic.cs
@@ -11,21 +11,45 @@ using Microsoft.Xna.Framework.Media;
 
 namespace PyramidPanic
 {
+    private enum GameState
+    { Start, Play }
 
     public class PyramidPanic : Microsoft.Xna.Framework.Game
     {
         // Fields
-        GraphicsDeviceManager graphics;
-        SpriteBatch spriteBatch;
+        private GraphicsDeviceManager graphics;
+        private SpriteBatch spriteBatch;
+        private Texture2D background;
 
+        //Keyboard voegt ie toe.
+        private KeyboardState keyboardState, oldKeyboardState;
+        //GameState standaard
+        private GameState state = GameState.Start;
+        
+
+        //Constructor
         public PyramidPanic()
         {
-            graphics = new GraphicsDeviceManager(this);
+            this.graphics = new GraphicsDeviceManager(this);
             Content.RootDirectory = "Content";
         }
 
         protected override void Initialize()
         {
+            // Voegt title toe
+            this.Window.Title = "Pyramid Panic";
+
+            // Maak Mouse viasble
+            IsMouseVisible = true;
+
+            // Veranderd de breedte van het canvas
+            this.graphics.PreferredBackBufferWidth = 640;
+
+            // veranderd de hoogte van het canvas
+            this.graphics.PreferredBackBufferHeight = 480;
+
+            // past te veranderingen toe van het canvas
+            this.graphics.ApplyChanges();
 
             base.Initialize();
         }
@@ -34,8 +58,8 @@ namespace PyramidPanic
         protected override void LoadContent()
         {
             // Create a new SpriteBatch, which can be used to draw textures.
-            spriteBatch = new SpriteBatch(GraphicsDevice);
-
+            this.spriteBatch = new SpriteBatch(GraphicsDevice);
+            this.background = Content.Load<Texture2D>(@"menu/Background");
             
         }
 
@@ -48,11 +72,15 @@ namespace PyramidPanic
         
         protected override void Update(GameTime gameTime)
         {
+            //Kijkt iedere update of de toetsebord word gebruikt
+            this.keyboardState = Keyboard.GetState();
+
             // Allows the game to exit
-            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed)
+            if ((GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed) || (Keyboard.GetState().IsKeyDown(Keys.Escape)))
                 this.Exit();
 
-
+            //Voor edgedecetion, zet de huidige keyboardstate in oude keyboardstate.
+            this.oldKeyboardState = this.keyboardState;
             base.Update(gameTime);
         }
 
@@ -60,7 +88,9 @@ namespace PyramidPanic
         protected override void Draw(GameTime gameTime)
         {
             GraphicsDevice.Clear(Color.CornflowerBlue);
-
+            this.spriteBatch.Begin();
+            this.spriteBatch.Draw(this.background,new Rectangle(0,0,640,480), Color.FloralWhite);
+            this.spriteBatch.End();
             
             base.Draw(gameTime);
         }
