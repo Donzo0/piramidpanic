809dc3d6f847e4e26b60e7268fd90e78f640531e
Jason Binneveld <j.binneveld@live.nl>
2014-01-09 12:24:25 +0100

    Toets1 Commit 1

---
+++ PyramidPanic/PyramidPanic/GameScenes/HelpScene/ToetsScene.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+
+namespace PyramidPanic.GameScenes.HelpScene
+{
+    public class ToetsScene : IGameState
+    {
+                //Fields
+        private PyramidPanic game;
+
+        //Constructor
+        public ToetsScene(PyramidPanic game)
+        {
+            this.game = game;
+            this.Initialize();
+        }
+        //Initialize
+        public void Initialize()
+        {
+            this.LoadContent();
+        }
+        //LoadContent
+        public void LoadContent()
+        {
+        }
+        //Update
+        public void Update(GameTime gameTime)
+        {
+            if (Input.EdgeDetectKeyDown(Keys.B))
+            {
+                this.game.GameState = this.game.StartScene;
+            }
+        }
+        //Draw
+        public void Draw(GameTime gameTime)
+        {
+            this.game.GraphicsDevice.Clear(Color.Red);
+        }
+    }
+}
