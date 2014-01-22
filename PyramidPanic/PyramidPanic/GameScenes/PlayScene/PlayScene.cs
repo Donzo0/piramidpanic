fc31dfaba191411e9f7ff0baae1c58cce962bcef
Jason Binneveld <j.binneveld@live.nl>
2014-01-22 15:06:39 +0100

    Beetle class ingevuld en iBeetleClass ingevuld. WalkUp en WalkDOwn gemaakt voor beetle

--- PyramidPanic/PyramidPanic/GameScenes/PlayScene/PlayScene.cs
+++ PyramidPanic/PyramidPanic/GameScenes/PlayScene/PlayScene.cs
@@ -15,6 +15,7 @@ namespace PyramidPanic
     {
         //Fields
         private PyramidPanic game;
+        private Beetle beetle;
 
         //Constructor
         public PlayScene(PyramidPanic game)
@@ -30,6 +31,7 @@ namespace PyramidPanic
         //LoadContent
         public void LoadContent()
         {
+            this.beetle = new Beetle(this.game, new Vector2(200f, 300f));
         }
         //Update
         public void Update(GameTime gameTime)
@@ -38,11 +40,13 @@ namespace PyramidPanic
             {
                 this.game.GameState = this.game.StartScene;
             }
+            this.beetle.Update(gameTime);
         }
         //Draw
         public void Draw(GameTime gameTime)
         {
             this.game.GraphicsDevice.Clear(Color.Red);
+            this.beetle.Draw(gameTime);
         }
     }
 }
